2bc07fa74e8628e6ca568b29f98f7a674aea9fb2
LilBudj <kinich2001@gmail.com>
2020-03-04 18:07:00 +0300

    polaris stylizing

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,22 @@
+import {Layout, Page, TextStyle, EmptyState} from '@shopify/polaris'
+
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
+
 const Initial = () => (
     <div>
-        <p>Sample App</p>
+        <Page>
+            <Layout>
+       <TextStyle variation={"positive"}>
+           Sample App
+       </TextStyle>
+                <EmptyState heading={"Discounts!"} action={{
+                    content: 'Select Products',
+                    onAction: () => alert('clicked'),
+                }} image={img}>
+                    <p>Select products</p>
+                </EmptyState>
+            </Layout>
+        </Page>
     </div>
 );
 
