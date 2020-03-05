a91dabd70f817f7154e9b20d5d63622fc96229df
LilBudj <kinich2001@gmail.com>
2020-03-05 17:54:13 +0300

    Navigation + Auth

--- pages/_app.js
+++ pages/_app.js
@@ -3,18 +3,23 @@ import Head from 'next/head';
 import {AppProvider} from "@shopify/polaris";
 import '@shopify/polaris/styles.css'
 import translations from '@shopify/polaris/locales/en.json'
+import {Provider} from "@shopify/app-bridge-react";
+import Cookies from 'js-cookie'
 
 class MyApp extends App{
     render(){
+        const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
         return(
             <React.Fragment>
                 <Head>
                     <title>Sample App</title>
                     <meta charSet="utf-8"/>
                 </Head>
+                <Provider config={config}>
                 <AppProvider i18n={translations}>
-                <props.Component {...this.props.pageProps}/>
+                <this.props.Component {...this.props.pageProps}/>
                 </AppProvider>
+                </Provider>
             </React.Fragment>
         )
     }
