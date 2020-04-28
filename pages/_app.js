d77458709438e2ed88e7d243fb37468ba88a47cd
LilBudj <kinich2001@gmail.com>
2020-04-28 17:06:22 +0300

    next build attempt [15]

--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,14 @@ import '@shopify/polaris/styles.css'
 import translations from '@shopify/polaris/locales/en.json'
 import {Provider} from "@shopify/app-bridge-react";
 import Cookies from 'js-cookie'
-import ApolloClient from 'react-apollo'
+import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 
-// const client = new ApolloClient({
-//      fetchOptions: {
-//          credentials: 'include'
-//      }
-// });
+const client = new ApolloClient({
+      fetchOptions: {
+          credentials: 'include'
+      }
+ });
 
 class MyApp extends App{
     componentDidMount(){
@@ -28,9 +28,9 @@ class MyApp extends App{
                 </Head>
                 <Provider config={config}>
                 <AppProvider i18n={translations}>
-                    {/*<ApolloProvider client={client}>*/}
+                    <ApolloProvider client={client}>
                 <this.props.Component {...this.props.pageProps}/>
-                    {/*</ApolloProvider>*/}
+                    </ApolloProvider>
                 </AppProvider>
                 </Provider>
             </React.Fragment>
