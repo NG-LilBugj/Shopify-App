3267527593d827909c7a92bdcbf2b7deb0fb470c
LilBudj <kinich2001@gmail.com>
2020-04-29 15:35:23 +0300

    api experiments

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-import axios from "axios";
-
 require('isomorphic-fetch');
 const dotenv = require('dotenv');
 const Koa = require('koa');
@@ -11,6 +9,7 @@ const lusca = require('koa-lusca');
 const {default: createShopifyAuth} = require('@shopify/koa-shopify-auth');
 const {verifyRequest} = require('@shopify/koa-shopify-auth');
 const session = require('koa-session');
+const axios = require('axios');
 
 dotenv.config();
 
@@ -97,7 +96,7 @@ app.prepare().then(() => {
         ctx.res.statusCode = 200;
     });
 
-    axios.get('https://nahku-b-tahke.myshopify.com/admin/api/2020-04/script_tags.json').then(res => {local.push(res.data)})
+    axios.get('https://nahku-b-tahke.myshopify.com/admin/api/2020-04/script_tags.json').then(res => {local.push(res.data)});
 
     server.listen(port, () => {
         console.log(`App is ready on port ${port}`)
