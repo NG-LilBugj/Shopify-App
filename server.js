466b055b893e3d509c9a89222b8a23257fa5bb9f
LilBudj <kinich2001@gmail.com>
2020-05-04 16:57:27 +0300

    test delete

--- server.js
+++ server.js
@@ -78,8 +78,13 @@ router.post('/api/script', koaBody(), async (ctx) => {
 });
 router.delete('/api/script', koaBody(), async (ctx) => {
     try {
-        config.pop();
-        axios.delete(`https://nahku-b-tahke.myshopify.com/admin/api/2020-04/script_tags/${config[0].id}.json`, {
+        let elem = config.pop();
+        axios.delete(`https://nahku-b-tahke.myshopify.com/admin/api/2020-04/script_tags/125896327303.json`, {
+            headers: {
+                "X-Shopify-Access-Token": ctx.cookies.get('accessToken')
+            }
+        }).then(res => console.log(res));
+        axios.delete(`https://nahku-b-tahke.myshopify.com/admin/api/2020-04/script_tags/125896753287.json`, {
             headers: {
                 "X-Shopify-Access-Token": ctx.cookies.get('accessToken')
             }
