031d0bafc4a459e41f168d92ea8d58d94a46b080
LilBudj <kinich2001@gmail.com>
2020-05-05 22:05:44 +0300

    beta test

--- public/script.js
+++ public/script.js
@@ -1,5 +1,4 @@
-fetch('https://lil-shopify.herokuapp.com/auth')
-     .then(res => res.json())
+fetch('https://lil-shopify.herokuapp.com/api/config')
      .then(res => {res.data.name?renderTimer(res.data):renderTimer({
          endDate: { end:"Fri Jun 12 2020 00:00:00 GMT-0300" },
          position: "Top",
