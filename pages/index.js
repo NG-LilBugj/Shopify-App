5d6ab2870ec76f4b46e93a3631099838b06a6fed
LilBudj <kinich2001@gmail.com>
2020-05-05 15:09:19 +0300

    beta test

--- pages/index.js
+++ pages/index.js
@@ -136,8 +136,12 @@ const Initial = () => {
                 </Layout.Section>}
                 {!!scriptData.config && <Layout.Section>
                     <Card title={"Existing Banner:"} sectioned>
-                        <div style={{width: "100%", display: "flex", justifyContent: "space-between"}}>
-                        <p>{scriptData.script[0].configData?renderData(scriptData.script[0].configData.name):""}</p>
+                        <div style={{width: "100%", display: "flex", justifyContent: "space-between", padding: '10px'}}>
+                            <p>Banner name:</p>
+                            <p>Actions:</p>
+                        </div>
+                        <div style={{width: "100%", display: "flex", justifyContent: "space-between", padding: '10px'}}>
+                        <p>{scriptData.script[0].configData?renderData(scriptData.script[0].configData.name):"Timer"}</p>
                         <Link href={'/'}>
                             <Button
                                 primary
@@ -147,7 +151,6 @@ const Initial = () => {
                                 Delete Banner
                             </Button>
                         </Link>
-                            <img style={{width: '64px', height: '64px', borderRadius: '32px'}} src={'https://img.icons8.com/cotton/2x/delete-sign--v2.png'} alt={'delete'}/>
                         </div>
                     </Card>
                 </Layout.Section>}
