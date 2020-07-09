5379c5463a053fad7f9ed41028e6e38ada76f5c1
LilBudj <kinich2001@gmail.com>
2020-07-09 12:22:56 +0300

    Search field fix

--- components/primary.js
+++ components/primary.js
@@ -153,7 +153,7 @@ const PrimaryDesign = (props) => {
             </Card>
             {(renderProduct) && <Card sectioned>
                 <Stack vertical>
-                    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between',width: '420px', height: '30px'}}>
+                    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between',width: '420px'}}>
                         <Autocomplete
                             onSelect={(value) => console.log(value)}
                             selected={[]}
