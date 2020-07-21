f76d54d4998b88c0b7a9ba0b43beb49acacb027c
LilBudj <kinich2001@gmail.com>
2020-07-21 13:13:21 +0300

    middleware page test[27]

--- components/initPage.js
+++ components/initPage.js
@@ -6,12 +6,16 @@ import {withStyles} from "@material-ui/core";
 const ShopifyButton = withStyles({
     root: {
         fontFamily: [
+            '-apple-system',
             'BlinkMacSystemFont',
             '"Segoe UI"',
             'Roboto',
             '"Helvetica Neue"',
             'Arial',
             'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
         ].join(','),
     }
 })(Button);
