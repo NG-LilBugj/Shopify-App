928c2196960650d45081e5d461a128e0cb87c855
LilBudj <kinich2001@gmail.com>
2020-07-28 12:23:35 +0300

    Page layout test [5]

--- components/bannerVariants.js
+++ components/bannerVariants.js
@@ -109,6 +109,7 @@ const BannerVariants = (props) => {
                             link={'/badges'}
                         />
                     }
+                    <Layout.Section>
                         <Card sectioned>
                             <div
                                 style={{display: "flex", justifyContent: "space-between", maxWidth: '540px', cursor: "pointer"}}
@@ -134,13 +135,17 @@ const BannerVariants = (props) => {
                                      alt={'design'}/>
                             </div>
                         </Card>
+                    </Layout.Section>
                     {props.bannerData[0].config &&
+                    <Layout.Section>
                         <BannerInfo
                             data={props.bannerData[0]}
                             link={'/animations'}
                         />
+                    </Layout.Section>
                     }
                 </div>
+                <Layout.Section>
                     <div style={{
                         display: "flex",
                         justifyContent: "space-evenly",
@@ -165,6 +170,7 @@ const BannerVariants = (props) => {
                             </TextStyle>
                         </DisplayText>}
                     </div>
+                </Layout.Section>
             </Layout>
         </Page>
     )
