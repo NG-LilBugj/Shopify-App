bd0add5355c48bbc865bc93287e032e5c76340fa
LilBudj <kinich2001@gmail.com>
2020-08-03 13:07:52 +0300

    Warn banner display fix [9]

--- redux/configsReducer.js
+++ redux/configsReducer.js
@@ -198,7 +198,6 @@ export const configsReducer = (state = initState, action) => {
                 };
         }
         case HANDLE_SALE_PRODUCTS: {
-            console.log(action);
             let arr = state.countdownConfig.script.filter(s => s.id !== state.dispatchedIds.countdownId)
                 .map(s => s.configData.products.map(c => action.products.filter(a => c.id === a.id)).map(e => e[0]))[0]
                 .filter(e => Boolean(e));
@@ -246,6 +245,7 @@ export const configsReducer = (state = initState, action) => {
             let arr = state.countdownConfig.script.filter(s => s.id !== state.dispatchedIds.countdownId)
                 .map(s => s.configData.products.map(c => action.products.filter(a => c.id === a.id)).map(e => e[0]))[0]
                 .filter(e => Boolean(e));
+            console.log('array:', arr);
             if (arr[0]) return {
                 ...state,
                 displayWarnings: {
