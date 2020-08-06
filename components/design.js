0c6c30c03627f3a25738ba13ed251a4decc8acaa
LilBudj <kinich2001@gmail.com>
2020-08-06 16:47:05 +0300

    design rework

--- components/design.js
+++ components/design.js
@@ -6,6 +6,56 @@ import {connect} from "react-redux";
 const DesignSection = (props) => {
     return(
         <Layout.Section>
+            <Card title={props.strings.bannerTexts} sectioned>
+                <TextField
+                    label={props.strings.firstText}
+                    value={props.firstText}
+                    onChange={(value) => {
+                        props.setFirstText(value)
+                    }}
+                    error={(!props.firstText) ? props.strings.pleaseEnterText : ''}
+                />
+                <div style={{marginTop: '25px'}}>
+                    <TextField
+                    label={props.strings.secondText}
+                    value={props.secondText}
+                    onChange={(value) => {
+                        props.setSecondText(value)
+                    }}
+                    error={(!props.secondText) ? props.strings.pleaseEnterText : ''}
+                />
+                </div>
+            </Card>
+            <Card sectioned>
+                <Checkbox
+                    label={props.strings.discountLink}
+                    checked={props.isLinkActive}
+                    onChange={(newChecked) => {
+                        props.activateLink(newChecked)
+                    }}
+                />
+                {props.isLinkActive && <div style={{display: 'flex', flexDirection: 'row', height: '60px'}}>
+                <TextField
+                    label={props.strings.title}
+                    value={props.linkText}
+                    onChange={(value) => {
+                        props.setLinkText(value)
+                    }}
+                    error={(!props.linkText) ? props.strings.pleaseEnterText : ''}
+                />
+                <TextField
+                    label={props.strings.link}
+                    value={props.href}
+                    onChange={(value) => {
+                        props.setHref(value)
+                    }}
+                    error={(!props.href) ? props.strings.pleaseEnterText : ''}
+                />
+                </div>}
+            </Card>
+            <Preview
+                decodeColors={decodeColors} {...props}
+            />
             <Card title={props.strings.timerDesign} sectioned>
                 <div style={{display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', width: '100%'}}>
                     <div>
@@ -61,56 +111,6 @@ const DesignSection = (props) => {
                     </div>
                 </div>
             </Card>
-            <Card title={props.strings.bannerTexts} sectioned>
-                <TextField
-                    label={props.strings.firstText}
-                    value={props.firstText}
-                    onChange={(value) => {
-                        props.setFirstText(value)
-                    }}
-                    error={(!props.firstText) ? props.strings.pleaseEnterText : ''}
-                />
-                <div style={{marginTop: '25px'}}>
-                    <TextField
-                    label={props.strings.secondText}
-                    value={props.secondText}
-                    onChange={(value) => {
-                        props.setSecondText(value)
-                    }}
-                    error={(!props.secondText) ? props.strings.pleaseEnterText : ''}
-                />
-                </div>
-            </Card>
-            <Card sectioned>
-                <Checkbox
-                    label={props.strings.discountLink}
-                    checked={props.isLinkActive}
-                    onChange={(newChecked) => {
-                        props.activateLink(newChecked)
-                    }}
-                />
-                {props.isLinkActive && <div style={{display: 'flex', flexDirection: 'row', height: '60px'}}>
-                <TextField
-                    label={props.strings.title}
-                    value={props.linkText}
-                    onChange={(value) => {
-                        props.setLinkText(value)
-                    }}
-                    error={(!props.linkText) ? props.strings.pleaseEnterText : ''}
-                />
-                <TextField
-                    label={props.strings.link}
-                    value={props.href}
-                    onChange={(value) => {
-                        props.setHref(value)
-                    }}
-                    error={(!props.href) ? props.strings.pleaseEnterText : ''}
-                />
-                </div>}
-            </Card>
-            <Preview
-                decodeColors={decodeColors} {...props}
-            />
         </Layout.Section>
     )
 };
