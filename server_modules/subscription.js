d4e62c7f7d8beb5e3e00c5a1a38c80411c17a80d
LilBudj <kinich2001@gmail.com>
2020-09-22 14:01:41 +0300

    subscription test

--- server_modules/subscription.js
+++ server_modules/subscription.js
@@ -2,22 +2,49 @@ const axios = require('axios');
 
 const getSubscriptionUrl = async (ctx, accessToken, shop) => {
 
-    const response = await axios.post(`https://${shop}/admin/api/2020-01/recurring_application_charges.json`, {
-        "recurring_application_charge": {
-            "name": "TopSale Banners Plan",
-            "price": 0.99,
-            "return_url": process.env.HOST,
-            "trial_days": 7,
+    const query = JSON.stringify({
+        query: `mutation {
+      appSubscriptionCreate(
+          name: "Free plan with priority"
+          returnUrl: "${process.env.HOST}"
+          test: true
+          lineItems: [
+          {
+            plan: {
+              appRecurringPricingDetails: {
+                  price: { amount: 0.99, currencyCode: USD }
+              }
+            }
+          }
+          {
+            plan: {
+              appRecurringPricingDetails: {
+                  price: { amount: 0, currencyCode: USD }
+              }
+            }
+          }
+          ]
+        ) {
+            userErrors {
+              field
+              message
+            }
+            confirmationUrl
+            appSubscription {
+              id
+            }
         }
-    },{
+    }`
+    });
+
+    const response = await axios.post(`https://${shop}/admin/api/2020-01/graphql.json`, query,{
         headers: {
             "X-Shopify-Access-Token": accessToken,
         },
     });
 
     const responseJson = await response;
-    const confirmationUrl = responseJson.data.recurring_application_charge.confirmation_url;
-    console.log(confirmationUrl);
+    const confirmationUrl = responseJson.data.appSubscriptionCreate.confirmation_url;
     return ctx.redirect(confirmationUrl)
 };
 
