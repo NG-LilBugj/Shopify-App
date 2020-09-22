96dd99347063e9029b855c9607941b4081e6f703
LilBudj <kinich2001@gmail.com>
2020-09-22 14:54:29 +0300

    subscription test [9]

--- server_modules/subscription.js
+++ server_modules/subscription.js
@@ -38,19 +38,22 @@ const getSubscriptionUrl = async (ctx, accessToken, shop) => {
     });
 
     try {
-        const response = await axios.post(`https://${shop}/admin/api/2019-10/graphql.json`, {body: query}, {
+        const response = await fetch(`https://${shop}/admin/api/2019-10/graphql.json`, {
+            method: 'POST',
             headers: {
+                'Content-Type': 'application/json',
                 "X-Shopify-Access-Token": accessToken,
             },
+            body: query
         });
 
-        const responseJson = await response;
+        const responseJson = await response.json();
         const confirmationUrl = responseJson.data.appSubscriptionCreate.confirmationUrl;
         console.log(confirmationUrl);
         return ctx.redirect(confirmationUrl)
     }
     catch (e) {
-        console.log(e.data)
+        console.log(e)
     }
 };
 
