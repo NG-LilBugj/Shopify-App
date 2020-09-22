773ee39741b82c294732bc955b4b8e05c527a854
LilBudj <kinich2001@gmail.com>
2020-09-22 11:31:28 +0300

    descent webhooking light test

--- server_modules/uninstallWebhook.js
+++ server_modules/uninstallWebhook.js
@@ -5,51 +5,52 @@ let {BannerConfig, BadgeConfig, AnimationConfig} = DBAccess;
 
 const uninstallWebhook = async (ctx) => {
         console.log('webhook fetched!:', ctx.state.webhook);
-        BannerConfig.find({shop: ctx.cookies.get('shopOrigin')}, (err, res) => {
-            if (err) {
-                console.log(err)
-            }
-            else {
-                BannerConfig.delete(res, err => console.log(err))
-            }
-        });
-        BadgeConfig.find({shop: ctx.cookies.get('shopOrigin')}, (err, res) => {
-            if (err) {
-                console.log(err)
-            }
-            else {
-                BadgeConfig.delete(res, err => console.log(err))
-            }
-        });
-        AnimationConfig.find({shop: ctx.cookies.get('shopOrigin')}, (err, res) => {
-            if (err) {
-                console.log(err)
-            }
-            else {
-                AnimationConfig.delete(res, err => console.log(err))
-            }
-        });
+        // BannerConfig.find({shop: ctx.cookies.get('shopOrigin')}, (err, res) => {
+        //     if (err) {
+        //         console.log(err)
+        //     }
+        //     else {
+        //         BannerConfig.delete(res, err => console.log(err))
+        //     }
+        // });
+        // BadgeConfig.find({shop: ctx.cookies.get('shopOrigin')}, (err, res) => {
+        //     if (err) {
+        //         console.log(err)
+        //     }
+        //     else {
+        //         BadgeConfig.delete(res, err => console.log(err))
+        //     }
+        // });
+        // AnimationConfig.find({shop: ctx.cookies.get('shopOrigin')}, (err, res) => {
+        //     if (err) {
+        //         console.log(err)
+        //     }
+        //     else {
+        //         AnimationConfig.delete(res, err => console.log(err))
+        //     }
+        // });
 
         await end.amplitudeUninstallEvent(ctx);
+        ctx.body = {web: ctx.state.webhook}
 
-        axios.get(`https://${ctx.cookies.get('shopOrigin')}/admin/api/2020-01/recurring_application_charges.json`, {
-            headers: {
-                "X-Shopify-Access-Token": ctx.cookies.get('accessToken'),
-            },
-        })
-            .then(res => {
-                res.data.recurring_application_charges.forEach(e => {
-                    if (e.return_url === "https://lil-shopify.herokuapp.com/") {
-                        axios.delete(`https://${ctx.cookies.get('shopOrigin')}/admin/api/2020-01/recurring_application_charges/${e.id}.json`, {
-                            headers: {
-                                "X-Shopify-Access-Token": ctx.cookies.get('accessToken'),
-                            },
-                        }).catch(e => console.log(e))
-                    }
-                });
-                ctx.body = {web: ctx.state.webhook}
-            })
-            .catch(e => console.log(e));
+        // axios.get(`https://${ctx.cookies.get('shopOrigin')}/admin/api/2020-01/recurring_application_charges.json`, {
+        //     headers: {
+        //         "X-Shopify-Access-Token": ctx.cookies.get('accessToken'),
+        //     },
+        // })
+        //     .then(res => {
+        //         res.data.recurring_application_charges.forEach(e => {
+        //             if (e.return_url === "https://lil-shopify.herokuapp.com/") {
+        //                 axios.delete(`https://${ctx.cookies.get('shopOrigin')}/admin/api/2020-01/recurring_application_charges/${e.id}.json`, {
+        //                     headers: {
+        //                         "X-Shopify-Access-Token": ctx.cookies.get('accessToken'),
+        //                     },
+        //                 }).catch(e => console.log(e))
+        //             }
+        //         });
+        //         ctx.body = {web: ctx.state.webhook}
+        //     })
+        //     .catch(e => console.log(e));
 };
 
 module.exports = uninstallWebhook;
