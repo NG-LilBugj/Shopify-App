47250615098e063e65aa3a72e80b4ff6a0210f89
LilBudj <kinich2001@gmail.com>
2020-09-24 17:47:54 +0300

    subscription fixes [2]

--- server_modules/subscription.js
+++ server_modules/subscription.js
@@ -2,20 +2,25 @@ const axios = require('axios');
 
 const getSubscriptionUrl = async (ctx, accessToken, shop) => {
 
+    try {
         const response = axios.post(`https://${shop}/admin/api/2020-07/recurring_application_charges.json`, {
-            "name": "Super Duper Plan",
+            "name": "TopSale Banners Plan",
             "price": 0.99,
             "return_url": process.env.HOST,
             "trial_days": 7
-            }, {
+        }, {
             headers: {
                 "X-Shopify-Access-Token": accessToken,
             },
         });
+
         const responseJson = await response;
         const confirmationUrl = responseJson.data.recurring_application_charge.confirmation_url;
         return ctx.redirect(confirmationUrl)
-
+    }
+    catch (e) {
+        console.log(e)
+    }
 };
 
 module.exports = getSubscriptionUrl;
